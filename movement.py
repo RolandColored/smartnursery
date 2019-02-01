4c5803fd25917465321b4ea0773d016ebd475a18
Roland Kahlert <roland.kahlert@de.pwc.com>
2019-02-01 19:52:18 +0100

    add music control based on movement detection

--- movement.py
+++ movement.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import requests
+
 try:
     import RPi.GPIO as GPIO
 except ImportError:
@@ -7,9 +11,37 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO_PIR = 4
 
+musiccast_endpoint = 'http://Kinderzimmer/YamahaExtendedControl/v1/main/'
+last_movement = datetime.now()
+playing_music = False
+
+
+def play_music():
+    global playing_music
+
+    if not playing_music:
+        print('Playing music')
+        assert requests.get(musiccast_endpoint + 'setPower?power=on').status_code == 200
+        assert requests.get(musiccast_endpoint + 'setInput?input=net_radio').status_code == 200
+        assert requests.get(musiccast_endpoint + 'setPlayback?playback=play').status_code == 200
+        playing_music = True
+
+
+def stop_music():
+    global playing_music
+
+    if playing_music:
+        print('Stopping music')
+        assert requests.get(musiccast_endpoint + 'setPower?power=standby').status_code == 200
+        playing_music = False
+
 
 def movement_callback(_):
+    global last_movement
+
     print('moved')
+    last_movement = datetime.now()
+    play_music()
 
 
 if __name__ == '__main__':
@@ -19,8 +51,12 @@ if __name__ == '__main__':
         print('Started')
 
         while True:
-            time.sleep(1)
+            if (datetime.now() - last_movement).seconds > 60:
+                stop_music()
+
+            time.sleep(0.1)
 
     finally:
         print('Cleaning up')
+        stop_music()
         GPIO.cleanup()
