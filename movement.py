60de5d17945358341e7c229eb39d33c651023f87
Roland Kahlert <roland.kahlert@de.pwc.com>
2019-02-07 17:38:37 +0100

    add rf power plug control

--- movement.py
+++ movement.py
@@ -7,13 +7,24 @@ try:
 except ImportError:
     from fake_rpi.RPi import GPIO
 import time
+from rpi_rf import RFDevice
 
+# movement detector
 GPIO.setmode(GPIO.BCM)
 GPIO_PIR = 4
-
-musiccast_endpoint = 'http://Kinderzimmer/YamahaExtendedControl/v1/main/'
 last_movement = datetime.now()
+
+# music device
+musiccast_endpoint = 'http://Kinderzimmer/YamahaExtendedControl/v1/main/'
 playing_music = False
+
+# power switch
+enable_code = 5506385
+disable_code = 5506388
+
+GPIO_RF = 17
+rfdevice = RFDevice(GPIO_RF)
+rfdevice.enable_tx()
 
 
 def play_music():
@@ -36,12 +47,18 @@ def stop_music():
         playing_music = False
 
 
+def switch_lamp(code):
+    print('Sending code ' + str(code))
+    rfdevice.tx_code(code, 1, 350, 24)
+
+
 def movement_callback(_):
     global last_movement
 
-    print('moved')
+    print('Mobement detected')
     last_movement = datetime.now()
     play_music()
+    switch_lamp(enable_code)
 
 
 if __name__ == '__main__':
@@ -53,6 +70,7 @@ if __name__ == '__main__':
         while True:
             if (datetime.now() - last_movement).seconds > 60:
                 stop_music()
+                switch_lamp(disable_code)
 
             time.sleep(0.1)
 
